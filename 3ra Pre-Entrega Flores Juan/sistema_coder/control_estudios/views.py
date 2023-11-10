04c04555b16f0135ce2390c848d64d6c57deec5d
Juan Esteban Flores <147572449+sache216@users.noreply.github.com>
2023-11-09 21:56:10 -0300

    Add files via upload

---
+++ 3ra Pre-Entrega Flores Juan/sistema_coder/control_estudios/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from control_estudios.models import Estudiante
+
+
+def listar_estudiantes(request):
+    contexto = {
+        "profesor": "Juan",
+        "estudiantes": Estudiante.objects.all(),
+    }
+    http_response = render(
+        request=request,
+        template_name='control_estudios/lista_estudiantes.html',
+        context=contexto,
+    )
+    return http_response
